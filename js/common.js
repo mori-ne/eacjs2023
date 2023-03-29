5b916edec1a5389f064d0686a9de233b728d70e5
mori-ne <fold.it.fold.it@gmail.com>
2023-03-29 16:25:53 +0900

    [0329]

--- js/common.js
+++ js/common.js
@@ -1,11 +1,32 @@
-const box = $("#onhover")
+// ナビゲーションメニューのホバー
 $(function () {
-	box.hover(
-		function () {
-			$(this).css('background', 'red');
-		},
-		function () {
-			$(this).css('background', 'orange');
+	$(".onhover").hover(function () {
+		subHeight = $(".dropdown_list").height();
+		$(".onhover").removeClass("active");
+		if (subHeight <= 0) {
+			$(".dropdown_list").addClass("active");
+		} else {
+			$(".dropdown_list").removeClass("active");
 		}
-	)
+	});
+});
+
+$(function () {
+	$(".onhover2").hover(function () {
+		subHeight = $(".dropdown_list2").height();
+		$(".onhover2").removeClass("active");
+		if (subHeight <= 0) {
+			$(".dropdown_list2").addClass("active");
+		} else {
+			$(".dropdown_list2").removeClass("active");
+		}
+	});
+});
+
+// お知らせのトグル
+$(function () {
+	$('.nav-open').click(function () {
+		$(this).toggleClass('show');
+		$(this).next('nav').slideToggle(300);
+	});
 });
