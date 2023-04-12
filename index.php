a41ec2642307f1788f6d2d0ce3f97934e29d81d8
mori-ne <fold.it.fold.it@gmail.com>
2023-04-12 11:02:29 +0900

    [0412]アロー追加

--- index.php
+++ index.php
@@ -342,35 +342,10 @@ require_once("head.php");
 
 
 
-		<footer class="footer" id="footer">
-			<div class="footer_inner">
-
-				<div class="footer_box">
-					<div class="footer_logo">
-						<img src="img/logo_white.svg" alt="東アジア日本研究者協議会 第7回国際学術大会">
-					</div>
-				</div>
-
-				<div class="footer_box">
-					<h5 class="footer_ttl">連絡先</h5>
-					<p>
-						第7回国際学術大会委員長　○○○○<br>
-						○○○○○○○○○○○長：○○○○<br>
-						Tel: 000-000-0000 Fax: 000-000-0000<br>
-						E-mail: info@eacjs2023.jp
-					</p>
-				</div>
-
-				<div class="footer_box">
-					<h5 class="footer_ttl">東京外国語大学</h5>
-					<p>
-						〒183-0003 東京都府中市朝日町3-11-1<br>
-						Tel: 042-330-5111
-					</p>
-				</div>
-
-			</div>
-		</footer>
+		<?php
+		// フッター
+		require_once("footer.php");
+		?>
 
 
 
