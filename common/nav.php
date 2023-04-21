a95c5cecf5c131accbb73c85f4bdc89e8eb9aa42
mori-ne <fold.it.fold.it@gmail.com>
2023-04-21 17:09:51 +0900

    [0421]コメントアウト削除

--- common/nav.php
+++ common/nav.php
@@ -40,24 +40,4 @@
 
 	</div>
 
-	<!-- <div class="dropdown_list">
-		<ul class="dropdown_list_inner">
-			<li><a href="./p_panel.php">パネル発表</a></li>
-			<li><a href="./p_abstract.php">個人研究発表</a></li>
-			<li><a href="./p_nextgenpanel.php">次世代パネル発表</a></li>
-			<li><a href="./p_nextgenabstract.php">次世代研究発表</a></li>
-			<li><a href="./p_public.php">一般参加<br>（参観のみ）</a></li>
-		</ul>
-	</div> -->
-
-	<!-- <div class="dropdown_list2">
-		<ul class="dropdown_list_inner2">
-			<li><a href="#">パネル発表</a></li>
-			<li><a href="#">個人研究発表</a></li>
-			<li><a href="#">次世代パネル発表</a></li>
-			<li><a href="#">次世代研究発表</a></li>
-			<li><a href="#">一般参加<br>（参観のみ）</a></li>
-		</ul>
-	</div> -->
-
 </nav>
