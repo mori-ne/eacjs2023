48763b96370dc673cd4e72432489c8f6b37d3580
mori-ne <fold.it.fold.it@gmail.com>
2023-05-23 15:14:43 +0900

    [0523]favicon、インデックス、メタタグ設定

--- p_panel.php
+++ p_panel.php
@@ -12,7 +12,11 @@ $separetor = " | ";
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link href="img/favicon.ico" rel="icon">
+	<link href="img/favicon.ico" rel="apple-touch-icon">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="本大会は、東アジアを中心とした国々の研究者に、多様な分野において蓄積されてきた日本研究に関する成果を発表・議論する場を提供し、日本研究と日本研究者の国際的交流の発展に寄与することを目的としています。" />
+	<meta content="東アジア日本研究者協議会,第7回国際学術大会,East Asian Consortium of Japanese Studies" name="keywords">
 	<title><?php print($sub_pagettl . $separetor); ?>東アジア日本研究者協議会 第7回国際学術大会</title>
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/swiper.min.css">
